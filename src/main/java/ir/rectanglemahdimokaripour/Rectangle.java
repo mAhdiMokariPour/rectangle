a194ba8aaa009242761be42a0c0f545da07f4059
Mahdi Mokari <mahdimokari3333@gmail.com>
2023-04-18 22:17:40 +0430

    set methods declared!

--- src/main/java/ir/rectanglemahdimokaripour/Rectangle.java
+++ src/main/java/ir/rectanglemahdimokaripour/Rectangle.java
@@ -9,8 +9,19 @@ package ir.rectanglemahdimokaripour;
  * @author mAAhdi
  */
 public class Rectangle {
+    
+    private int width;
+    private int length;
+    
+    public void setWidth(int width){
+        this.width = width; 
+    }
+    
+    public void setLength(int length){
+        this.length = length;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
