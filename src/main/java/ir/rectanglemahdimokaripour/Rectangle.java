828a01a29d26cf6eb3a6c2025cd168e0aa030e15
Mahdi Mokari <mahdimokari3333@gmail.com>
2023-04-18 21:27:39 +0430

    Rectangle Project Created.

---
+++ src/main/java/ir/rectanglemahdimokaripour/Rectangle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ir.rectanglemahdimokaripour;
+
+/**
+ *
+ * @author mAAhdi
+ */
+public class Rectangle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
