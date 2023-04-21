953eb1ba1fa2fa771721daf7fdb729ecd2268e93
Mahdi Mokari <mahdimokari3333@gmail.com>
2023-04-21 10:05:45 +0430

    CalculateArea and CalculatePrimeter added to Rectangle.java

--- src/main/java/ir/rectanglemahdimokaripour/Rectangle.java
+++ src/main/java/ir/rectanglemahdimokaripour/Rectangle.java
@@ -20,8 +20,22 @@ public class Rectangle {
     public void setLength(int length){
         this.length = length;
     }
+    
+    public int calculateArea(){
+        return width*length;
+    }
+    
+    public int calculatePrimeter(){
+        return (width+length)*2;
+    }
 
     public static void main(String[] args) {
         
+        Rectangle rect1 = new Rectangle();
+        rect1.setLength(5);
+        rect1.setWidth(3);
+        
+        System.out.println("Rectangle Area is::" + rect1.calculateArea());
+        System.out.println("Rectangle Primeter is::" + rect1.calculatePrimeter());
     }
 }
